ded095d831b0379038031fb2e238ef6049a69948
Aren Chilingaryan <arenchilingaryan88@gmail.com>
2021-10-05 14:34:32 +0400

    map count of cars

--- cpp_task/cpp_task.cpp
+++ cpp_task/cpp_task.cpp
@@ -6,15 +6,26 @@
 using namespace std;
 
 int main() {
-	srand(time(0));
-	string array[6] = { "mercedes","opel","volvo","lambo","fera","gelik" };
+	const int size = 10;
+	string* array = new string[size]{ "bmw","mercedes","opel","volvo","lambo","fera", "fera","gelik", "bmw","opel" };
 	map<string, int> cars;
+	int count = 0;
 
-	for (int i = 0; i < 6; i++) {
-		cars.insert(pair<string, int>(array[i], rand() % 10));
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			if (array[i] == array[j]) {
+				count += 1;
+			}
+		}
+		if (cars.count(array[i]) == 0) {
+			cars.insert(pair<string, int>(array[i], count));
+		}
+		count = 0;
 	}
 	map<string, int>::iterator iter;
 	for (iter = cars.begin(); iter != cars.end(); iter++) {
 		cout << iter->first << " = " << iter->second << endl;
 	}
 }
+
+
